6625e6a1f514254b6ba733b3b3568ec881b70441
brandonlopes <brandonnlopes@protonmail.com>
2025-03-24 10:31:40 -0400

    transparency + i3bar

--- install-debian.sh
+++ install-debian.sh
@@ -1,2 +1,2 @@
-sudo apt install -y lightdm i3 xorg xserver-xorg network-manager firefox-esr pulseaudio pavucontrol tlp && sudo systemctl enable --now tlp && sudo systemctl enable lightdm && sudo systemctl start lightdm
+sudo apt install -y lightdm i3 xorg xserver-xorg network-manager compton firefox-esr pulseaudio pavucontrol tlp && sudo systemctl enable --now tlp && sudo systemctl enable lightdm && sudo systemctl start lightdm
 
